baca28345f4a569b31484e149f131cbef6837c3d
helen-wli <helenwyue@gmail.com>
2022-06-01 19:37:54 -0700

    set up view controllers

---
+++ iRecipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iRecipe
+//
+//  Created by Helen Li on 5/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
