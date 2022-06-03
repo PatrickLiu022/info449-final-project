78b824a8ce3fb7ea27386d5e61575bda6c258291
helen-wli <helenwyue@gmail.com>
2022-06-02 22:48:21 -0700

    implemented fav/unfav feature and start recipe from favVC

--- iRecipe/ViewController/FavViewController.swift
+++ iRecipe/ViewController/FavViewController.swift
@@ -9,29 +9,29 @@ import UIKit
 
 class FavViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    //let favRecipes : [Recipe] = []
-    var favRecipeNames = ["fav recipe 1", "fav recipe 2", "fav recipe 3", "fav recipe 4"] // TODO: DELETE, uncomment above
-    var favRecipeDescs = ["description 1", "description 2", "description 3", "description 4"] // TODO: DELETE
-
-    
+//    //let favRecipes : [Recipe] = []
+//    var favRecipeNames = ["fav recipe 1", "fav recipe 2", "fav recipe 3", "fav recipe 4"] // TODO: DELETE, uncomment above
+//    var favRecipeDescs = ["description 1", "description 2", "description 3", "description 4"] // TODO: DELETE
     
     @IBOutlet weak var favTableView: UITableView!
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath) as! TableViewCell
-        //cell.recipeNameLabel.text = favRecipes[indexPath.row].recipeName
-        cell.recipeNameLabel.text = favRecipeNames[indexPath.row] // TODO: DELETE, uncommet above line
         
-        //cell.recipeDescLabel.text = favRecipes[indexPath.row].recipeDesc
-        cell.recipeDescLabel.text = favRecipeDescs[indexPath.row] // TODO: DELETE, uncommet above line
-
+        // TODO: Modify after fetching
+        //cell.recipeNameLabel.text = FavRecipes.instance.favRecipes[indexPath.row].recipeName
+        //cell.recipeDescLabel.text = FavRecipes.instance.favRecipes[indexPath.row].recipeDesc
+        cell.recipeNameLabel.text = FavRecipes.instance.favRecipeNames[indexPath.row]
+        
         return cell
     }
     
     // Defines the number of table cells being displayed
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return favRecipes.count
-        return favRecipeNames.count // TODO: DELETE, uncommet above line
+        
+        // TODO: Modify after fetching
+        //return FavRecipes.instance.favRecipes.count
+        return FavRecipes.instance.favRecipeNames.count
     }
     
     // Defines the height of each table cell
@@ -41,7 +41,11 @@ class FavViewController: UIViewController, UITableViewDataSource, UITableViewDel
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let recipeVC = storyboard?.instantiateViewController(withIdentifier: "recipeViewController") as? RecipeViewController {
-            recipeVC.currRecipeName = favRecipeNames[indexPath.row]
+            
+            // TODO: Modify after fetching
+            //recipeVC.currRecipe = FavRecipes.instance.favRecipes[indexPath.row]
+            recipeVC.currRecipeName = FavRecipes.instance.favRecipeNames[indexPath.row]
+            
             recipeVC.doneButtonDestination = "favViewController"
             self.navigationController?.pushViewController(recipeVC, animated: true)
         }
@@ -57,6 +61,12 @@ class FavViewController: UIViewController, UITableViewDataSource, UITableViewDel
                            forCellReuseIdentifier: "tableViewCell")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        favTableView.reloadData()
+    }
+
 
     /*
     // MARK: - Navigation
